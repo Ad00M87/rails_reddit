b496fca820fc1b90de5a012ca8299d959b466c59
Adam Kimball <ad00m@Adams-MacBook-Pro.local>
2017-08-30 14:18:08 -0600

    subs, topics, and comments working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'subs#index'
   resources :subs do
-    resources :topics 
+    resources :topics
   end
 
+  scope 'topics/:topic_id', as: 'topic' do
+    resources :comments, only: [:new, :create]
+  end
 
   # get '/tacos/:taco_id' (This is your URI pattern), to: 'food_trucks#tacos' (This is the action), as: 'tacos' (This is a prefix)
 end
